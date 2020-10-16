0645c15dfb6d8842d808b5886015869a20a158a2
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 08:50:14 +0700

    Add files via upload

---
+++ src/KambingStatic.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Kambing Static & Konstanta
+ */
+public class KambingStatic {
+    
+    // NAMA_KAMBING sebagai konstanta
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak "
+                            + Mamalia.jumlahKambing);
+    }
+}
---
+++ src/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Kambing Static & Konstanta
+ */
+public class Mamalia {
+    // Variabel jumlahKambing dideklarasikan sebagai statik 
+    public static int jumlahKambing;
+    
+}
